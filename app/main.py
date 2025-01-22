a0498a439880da9563170c510ddb00eaf1eb2e1a
LevRozengart <levkonst2007@gmail.com>
2025-01-22 19:22:37 +0500

    some_func

--- app/main.py
+++ app/main.py
@@ -7,3 +7,32 @@ app = FastAPI()
 @app.get("/")
 def home_page():
     return {"message": "Hello!"}
+
+
+class Classmates(str, Enum):
+    classmate_1 = "Gleb"
+    classmate_2 = "Kostya"
+
+@app.get("/dodiki/{name}")
+async def get_eblans(name: Classmates):
+    if name.value == "Gleb":
+        return {"returned_eblan": name}
+    return {"returned_eblan": name}
+
+
+items = {
+    1: "ball",
+    2: "shoes"
+}
+
+item_in_stock = {
+    1: True,
+    2: False
+}
+
+@app.get("/item/{item_id}/status")
+async def read_item(item_id: int, in_stock: bool | None = None):
+    item = {"item": items[item_id]}
+    if in_stock:
+        item.update({"in_stock": item_in_stock[item_id]})
+    return item
