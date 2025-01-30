5289377137b03e230aaa11538d5bbe4aa9532486
LevRozengart <levkonst2007@gmail.com>
2025-01-30 22:47:46 +0500

    huy

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from pydantic import BaseModel
+from typing import Annotated
 
 
 app = FastAPI()
 
 
-class Task(BaseModel):
+class STaskAdd(BaseModel):
     name: str
-    description: str | None
+    description: str | None = None
 
 
-@app.get("/tasks")
-def get_tasks():
-    task = Task(name="Сделай дз по инфе")
-    return {"data": task}
+class STask(STaskAdd):
+    id: int
+
+tasks = []
+
+@app.post("/tasks")
+async def add_task(
+        task: Annotated[STaskAdd, Depends()]
+):
+    tasks.append(task)
+    return {"ok": True}
+
+# @app.get("/tasks")
+# def get_tasks():
+#     task = STask(name="Сделай дз по инфе")
+#     return {"data": task}
