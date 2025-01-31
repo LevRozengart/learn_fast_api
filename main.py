efc639b3c58e48230f1e17a4c9e68f0d6a033c36
LevRozengart <levkonst2007@gmail.com>
2025-01-31 21:06:10 +0500

    Some result

--- main.py
+++ main.py
@@ -1,2 +1,42 @@
-from fastapi import FastAPI()
+from fastapi import FastAPI
+from pydantic import BaseModel
+import json
 
+app = FastAPI()
+
+
+class SBook(BaseModel):
+    title: str
+    author: str
+    year: int | None = None
+
+@app.post("/books/")
+def add_book(bookmodel: SBook):
+    with open("book.json") as f:
+        lst_of_books: list = json.load(f)
+    lst_of_books.append({"id": len(lst_of_books) + 1,
+                         "title": bookmodel.title,
+                         "author": bookmodel.author,
+                         "year": bookmodel.year})
+    with open("book.json", "w") as f:
+        json.dump(lst_of_books, f, indent=2)
+    return {"id": len(lst_of_books),
+            "title": bookmodel.title,
+            "author": bookmodel.author,
+            "year": bookmodel.year}
+
+
+@app.get("/books/")
+def get_lst_of_books():
+    with open("book.json") as f:
+        lst_of_books = json.load(f)
+    return lst_of_books
+
+@app.get("/books/{book_id}")
+def get_by_id(book_id: int):
+    with open("book.json") as f:
+        lst_of_books = json.load(f)
+    for book in lst_of_books:
+        if book["id"] == book_id:
+            return book
+    return {"error": "book isn't found"}
