f1ecc57f3e7f25997f4034b0d56243df608472e1
LevRozengart <levkonst2007@gmail.com>
2025-02-01 16:32:45 +0500

    movied start

--- main.py
+++ main.py
@@ -1,43 +1,71 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from typing import Annotated
 from pydantic import BaseModel
 import json
+from json import JSONDecodeError
+
 
 app = FastAPI()
 
 
-class SBook(BaseModel):
+class SAddMovie(BaseModel):
     title: str
-    author: str
-    year: int | None = None
+    genre: str
+    year: int
 
-@app.post("/books/")
-def add_book(bookmodel: SBook):
-    with open("book.json") as f:
-        lst_of_books: list = json.load(f)
-    book_id = max([book["id"] for book in lst_of_books]) + 1
-    lst_of_books.append({"id": book_id,
-                         "title": bookmodel.title,
-                         "author": bookmodel.author,
-                         "year": bookmodel.year})
-    with open("book.json", "w") as f:
-        json.dump(lst_of_books, f, indent=2)
-    return {"id": book_id,
-            "title": bookmodel.title,
-            "author": bookmodel.author,
-            "year": bookmodel.year}
+@app.post("/movies/")
+def add_movie(moviemodel: SAddMovie):
+    try:
+        with open("movies.json") as f:
+            lst_of_movies = json.load(f)
+    except (JSONDecodeError, FileNotFoundError):
+        with open("movies.json", "w") as f:
+            lst_of_movies = list()
+    max_id = max([i["id"] for i in lst_of_movies])
+    moviemodel = dict(moviemodel)
+    moviemodel.update({"id": max_id+1})
+    lst_of_movies.append(moviemodel)
+    try:
+        with open("movies.json", "w") as f:
+            json.dump(lst_of_movies, f, indent=2)
+            return moviemodel
+    except Exception as e:
+        return {"error": str(e)}
 
 
-@app.get("/books/")
-def get_lst_of_books():
-    with open("book.json") as f:
-        lst_of_books = json.load(f)
-    return lst_of_books
+@app.get("/movies/")
+def get_movies(
+        title: str | None = None,
+        genre: str | None = None,
+        year: int | None = None
+):
+    try:
+        with open("movies.json") as f:
+            lst_of_movies = json.load(f)
+    except FileNotFoundError as e:
+        return {"error": str(e)}
 
-@app.get("/books/{book_id}")
-def get_by_id(book_id: int):
-    with open("book.json") as f:
-        lst_of_books = json.load(f)
-    for book in lst_of_books:
-        if book["id"] == book_id:
-            return book
-    return {"error": "book isn't found"}
+    if title:
+        lst_of_movies = [i for i in lst_of_movies if i["title"] == title]
+    if genre:
+        lst_of_movies = [i for i in lst_of_movies if i["genre"] == genre]
+    if year:
+        lst_of_movies = [i for i in lst_of_movies if i["year"] == year]
+
+    if lst_of_movies:
+        return lst_of_movies
+    else:
+        return {"error": "not movies for your query"}
+
+@app.get("/movies/{movie_id}")
+def get_movie_by_id(movie_id: int):
+    try:
+        with open("movies.json") as f:
+            lst_of_movies = json.load(f)
+    except (JSONDecodeError, FileNotFoundError) as e:
+        return {"error": str(e)}
+
+    for movie in lst_of_movies:
+        if movie["id"] == movie_id:
+            return movie
+    return {"error": "movie with this id is not found"}
